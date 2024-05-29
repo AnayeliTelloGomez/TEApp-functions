6154f57489d3c3a79b366684fd243691ff6a5c45
Anayeli <anayelitg01@gmail.com>
2024-05-29 14:18:15 -0600

    guardar estadistica completo

--- guardarEstadistica.cs
+++ guardarEstadistica.cs
@@ -8,28 +8,97 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
+using MySql.Data.MySqlClient;
+using Google.Protobuf.Reflection;
+
 namespace TEAPP
 {
     public static class guardarEstadistica
     {
+        class estadistica{
+            public string idact;
+            public string correctas;
+            public string incorrectas;
+            public string tiempo; 
+            
+        }
+
+        class Error{
+            public string mensaje;
+            public Error(string mensaje)
+            {
+                this.mensaje = mensaje;
+            }
+        }
+
+
         [FunctionName("guardarEstadistica")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            try
+            {
+                log.LogInformation("Alta usuario.");
+                //leer la solicitud HTTP y almacenar en una cadena
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                //pasar la cadena leída en la solicitud HTTP a un objeto ripo paramAltaPaciente
+                log.LogInformation("reqbody", requestBody);
+                estadistica data = JsonConvert.DeserializeObject<estadistica>(requestBody);
 
-            string name = req.Query["name"];
+                //leer los parametros para establecer conexión
+                string Server = Environment.GetEnvironmentVariable("Server");
+                string UserID = Environment.GetEnvironmentVariable("UserID");
+                string Password = Environment.GetEnvironmentVariable("Password");
+                string DB = Environment.GetEnvironmentVariable("DataBase");
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+                //Crear string de conexion a BD
+                string conDB = "Server=" + Server + ";UserID=" + UserID + ";Password=" + Password + ";" + "Database=" + DB + ";SslMode=Preferred;";
+                //crear conex[on]
+                var conexion = new MySqlConnection(conDB);
+                //abrir conexion
+                conexion.Open();
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                //iniciar transaccion para asegurar integridad de datos
+                MySqlTransaction transaccion = conexion.BeginTransaction();
 
-            return new OkObjectResult(responseMessage);
+                try
+                {
+                    //Crear la variable tipo comando en MySQL y asignarle sus caracteristicas
+                    var cmdGuardarEstadisticas = new MySqlCommand();
+                    cmdGuardarEstadisticas.Connection = conexion;
+                    cmdGuardarEstadisticas.Transaction = transaccion;
+                    cmdGuardarEstadisticas.CommandText = "UPDATE actividad_asignada SET correctas=@correctas, incorrectas=@incorrectas , tiempo=@tiempo, realizada = 1  WHERE idactividad = @idact";
+                    cmdGuardarEstadisticas.Parameters.AddWithValue("@idact",int.Parse(data.idact));
+                    cmdGuardarEstadisticas.Parameters.AddWithValue("@correctas", int.Parse(data.correctas));
+                    cmdGuardarEstadisticas.Parameters.AddWithValue("@incorrectas", int.Parse(data.incorrectas));
+                    float tiempo=float.Parse(data.tiempo);
+                    cmdGuardarEstadisticas.Parameters.AddWithValue("@tiempo", tiempo);
+                    
+
+                    //executar Query (non porque no devuelve datos)
+                    cmdGuardarEstadisticas.ExecuteNonQuery();
+
+                    transaccion.Commit();
+                    return new OkObjectResult(new {message="Estadistica registrada correctamente."});
+
+                }
+                catch (Exception e)
+                {
+                    transaccion.Rollback();
+                    throw new Exception(e.Message);
+                }
+                finally
+                {
+                    conexion.Close();
+                }
+                //return new OkObjectResult(new {message="hola"+float.Parse(data.tiempo)});
+            }
+            catch (Exception e){
+                Console.WriteLine(e.Message);
+                return new BadRequestObjectResult(JsonConvert.SerializeObject(new Error(e.Message)));
+            }
+
         }
     }
 }
